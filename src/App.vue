9fbf451450e3d6868775dfd47f1bdea932750c79
yemingrujing <yemingrujing@users.noreply.github.com>
2018-11-19 18:10:36 +0800

    格式修改

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-<div id="app">
-  <div id="app-in">
-    <img src="./assets/logo.png"/>
-    <h1>Hello App!</h1>
+  <div id="app">
+    <div id="app-in">
+      <img src="./assets/logo.png"/>
+      <h1>Hello App!</h1>
+    </div>
+    <ul>
+       <li>
+         <router-link to="/first">点我跳转到第一页</router-link>
+       </li>
+       <li>
+         <router-link to="/second">点我跳转到第二页</router-link>
+       </li>
+     </ul>
+     <router-view class="view"></router-view>
+     <firstcomponent></firstcomponent>
   </div>
-  <ul>
-     <li>
-       <router-link to="/first">点我跳转到第一页</router-link>
-     </li>
-     <li>
-       <router-link to="/second">点我跳转到第二页</router-link>
-     </li>
-   </ul>
-   <router-view class="view"></router-view>
-   <firstcomponent></firstcomponent>
-</div>
 </template>
 
 <script>
